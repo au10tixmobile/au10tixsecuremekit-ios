d52f048bd44e42072b41fc4833f39171c3143b21
au10tixmobile-pipeline <au10tixmobile@au10tix.com>
2024-06-24 09:16:37 +0000

    Updating Package.swift to version 4.1.0 automatically from ios pipeline

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
     targets: [
         .binaryTarget(
           name: "Au10tixSecureMeKit",
-          url: "https://github.com/au10tixmobile/Au10tixSecureMeKit/archive/refs/tags/4.0.0.zip",
-          checksum: "89e3a83fcba32eea32439f6e457e923c0c5e4a340b820783cef6a3c5ac90749c"
+          url: "https://github.com/au10tixmobile/Au10tixSecureMeKit/archive/refs/tags/4.1.0.zip",
+          checksum: "d282a2850934f5298395f577a440c3f0458fa50911ee7b4d030b614a623fe90e"
         )
     ]
 )
